b08ae4e3fe47880851daf60fc0838c554ebfeed2
play.js <git@gituser.com>
2021-11-27 18:53:29 -0500

    file structure

---
+++ Src/components/new.js
@@ -0,0 +1,13 @@
+/** 
+
+When possible each component should be defined in its own dedicated file (SFC)
+Single File components should be named in PascalCase
+Base components should all start with the same prefix (like Base or App)
+You can think of base components as your app-wide reusable components like a button or a modal
+This groups them together and declares their global, reusable nature
+Component names should always be multi-worded to not conflict with any existing or future HTML elements. Don't create a Table or a Button component.
+Single instance components should begin with the prefix The
+For example a site header or footer
+This groups them together and declares them as single use
+Tightly coupled child components should be prefixed with their parent component's name
+**/
